b9d9f25cbd686fc2bec4342aa485441253333021
Mwakanemela <kayangemwakanemela@gmail.com>
2026-01-05 09:36:26 -0800

    unique name in uploads

--- upload.php
+++ upload.php
@@ -1,11 +1,21 @@
 <?php
 //make sure captured data exists
 if(isset($_FILES['files']) && !empty($_FILES["files"])) {
+
+    
     //upload destination directory(folder)
     $upload_destination = "uploads/";
 
     //move the temporary files to the new directory
     for($i = 0; $i < count($_FILES["files"]["tmp_name"]); $i++) {
+        /***
+     * 
+     * Validation
+     */
+    
+        //unique name
+        // $file = $upload_destination.md5(uniqid()).".".end(explode(".", $_FILES["files"]["name"][$i]))
+
         $file = $upload_destination.$_FILES['files']['name'][$i];
         move_uploaded_file($_FILES["files"]["tmp_name"][$i], $file);
     }
